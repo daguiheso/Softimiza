2c4bce9bb3a091f08278b1fc3d1c2336ad777fb9
Daniel H <frontend@reddesignsystems.com>
2016-09-01 15:44:43 -0500

    Add mediaquery js 600px

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+
+  var consulta =  window.matchMedia('(max-width:500px)'); 
+  consulta.addListener(mediaQuery);
+  mediaQuery ();
+
   // Counter
   var options = {
     useEasing : true, 
@@ -31,6 +36,91 @@ $(document).ready(function(){
   ];
   Materialize.scrollFire(optionsScroll);
 
+  
+    function mediaQuery () {
+      if (consulta.matches) {
+        console.log('se cumplio')
+        
+      }
+      else {
+        console.log('no se cumplio')
+        $('.bgimg-2')
+          .waypoint(function(direction) {
+            if (direction === 'down') {
+              $(this.element).addClass('animated fadeOut');
+              $(this.element).removeClass('fadeIn');
+              $(this.element).css('min-height', '60%')
+            }
+            else {
+              $(this.element).removeClass('fadeOut');
+              $(this.element).addClass('fadeIn');
+              $(this.element).css('min-height', '100%')
+            }
+          }, {
+            offset: -250
+          })
+
+        $('.bgimg-3')
+          .waypoint(function(direction) {
+            if (direction === 'down') {
+              $(this.element).addClass('animated fadeOut');
+              $(this.element).removeClass('fadeIn');
+              $(this.element).css('min-height', '60vh')
+            }
+            else {
+              $(this.element).removeClass('fadeOut');
+              $(this.element).addClass('fadeIn');
+              $(this.element).css('min-height', '80vh')
+            }
+          }, {
+            offset: '10px'
+          })
+
+        $('.bgimg-3')
+          .waypoint(function(direction) {
+            if (direction === 'down') {
+              $('#quote1').addClass('animated fadeIn');
+              $('#quote1').removeClass('fadeOut');
+            }
+            else {
+              $('#quote1').addClass('animated fadeOut');
+              $('#quote1').removeClass('fadeIn');
+            }
+          }, {
+            offset: '80%'
+          })
+
+        $('.bgimg-3')
+          .waypoint(function(direction) {
+            if (direction === 'down') {
+              $('#quote2').addClass('animated fadeIn');
+              $('#quote2').removeClass('fadeOut');
+            }
+            else {
+              $('#quote2').addClass('fadeOut');
+              $('#quote2').removeClass('fadeIn');
+            }
+          }, {
+            offset: '60%'
+          })
+
+        $('.bgimg-3')
+          .waypoint(function(direction) {
+            if (direction === 'down') {
+              $('#quote3').addClass('animated fadeIn');
+              $('#quote3').removeClass('fadeOut');
+            }
+            else {
+              $('#quote3').addClass('fadeOut');
+              $('#quote3').removeClass('fadeIn');
+            }
+          }, {
+            offset: '40%'
+          })
+      }
+    }
+  
+
   // Waypoints
   $('.bgimg-1')
     .waypoint(function(direction) {
@@ -42,78 +132,6 @@ $(document).ready(function(){
       offset: '-10%'
     })
 
-  $('.bgimg-2')
-    .waypoint(function(direction) {
-      if (direction === 'down') {
-        $(this.element).addClass('animated fadeOut');
-        $(this.element).removeClass('fadeIn');
-        $(this.element).css('min-height', '60%')
-      }
-      else {
-        $(this.element).removeClass('fadeOut');
-        $(this.element).addClass('fadeIn');
-        $(this.element).css('min-height', '80%')
-      }
-    }, {
-      offset: -250
-    })
-
-  $('.bgimg-3')
-    .waypoint(function(direction) {
-      if (direction === 'down') {
-        $(this.element).addClass('animated fadeOut');
-        $(this.element).removeClass('fadeIn');
-        $(this.element).css('min-height', '60vh')
-      }
-      else {
-        $(this.element).removeClass('fadeOut');
-        $(this.element).addClass('fadeIn');
-        $(this.element).css('min-height', '80vh')
-      }
-    }, {
-      offset: '10px'
-    })
-
-  $('.bgimg-3')
-    .waypoint(function(direction) {
-      if (direction === 'down') {
-        $('#quote1').addClass('animated fadeIn');
-        $('#quote1').removeClass('fadeOut');
-      }
-      else {
-        $('#quote1').addClass('animated fadeOut');
-        $('#quote1').removeClass('fadeIn');
-      }
-    }, {
-      offset: '80%'
-    })
-
-  $('.bgimg-3')
-    .waypoint(function(direction) {
-      if (direction === 'down') {
-        $('#quote2').addClass('animated fadeIn');
-        $('#quote2').removeClass('fadeOut');
-      }
-      else {
-        $('#quote2').addClass('fadeOut');
-        $('#quote2').removeClass('fadeIn');
-      }
-    }, {
-      offset: '60%'
-    })
-
-  $('.bgimg-3')
-    .waypoint(function(direction) {
-      if (direction === 'down') {
-        $('#quote3').addClass('animated fadeIn');
-        $('#quote3').removeClass('fadeOut');
-      }
-      else {
-        $('#quote3').addClass('fadeOut');
-        $('#quote3').removeClass('fadeIn');
-      }
-    }, {
-      offset: '40%'
-    })
+  
 
 });
